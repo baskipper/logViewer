a91b5a13c3437d00f4c4a1879e74a948efb84f75
unknown <bskipper@al-bskipper-d01.commandalkon.local>
2015-01-14 12:05:27 -0600

    Raw data displays from selected file

---
+++ angular-seed/app/app.js
@@ -0,0 +1,41 @@
+'use strict';
+
+// Declare app level module which depends on views, and components
+var myapp = angular.module('myApp', [
+  'ngRoute',
+  'myApp.view1',
+  'myApp.view2',
+  'myApp.version'
+]).
+config(['$routeProvider', function($routeProvider) {
+  $routeProvider.otherwise({redirectTo: '/view1'});
+}]);
+
+
+myapp.controller('MainCtrl', function ($scope) {
+  $scope.showContent = function($fileContent){
+    $scope.content = $fileContent;
+  };
+});
+
+myapp.directive('onReadFile', function ($parse) {
+  return {
+    restrict: 'A',
+    scope: false,
+    link: function(scope, element, attrs) {
+      var fn = $parse(attrs.onReadFile);
+
+      element.on('change', function(onChangeEvent) {
+        var reader = new FileReader();
+
+        reader.onload = function(onLoadEvent) {
+          scope.$apply(function() {
+            fn(scope, {$fileContent:onLoadEvent.target.result});
+          });
+        };
+
+        reader.readAsText((onChangeEvent.srcElement || onChangeEvent.target).files[0]);
+      });
+    }
+  };
+});
