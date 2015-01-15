b5d9c58cd8d99535f77ea6aca172ec901f3d0ffc
Benjamin Skipper <BSkipper@commandalkon.com>
2015-01-15 15:31:12 -0600

    Added search functionality on date, level, and message fields

--- angular-seed/app/app.js
+++ angular-seed/app/app.js
@@ -33,22 +33,32 @@ myapp.controller('MainCtrl', function ($scope, $filter, ngTableParams) {
 
     $scope.tableParams = new ngTableParams({
       page: 1,            // show first page
-      count: 10           // count per page
+      count: 10,          // count per page
+      filter: {
+
+        timestamp: '',
+        message: '',
+        level: ''
+
+      }
     }, {
       total: data.length, // length of data
 
 
 
       getData: function($defer, params) {
-
         // use build-in angular filter
+        var filteredData = params.filter() ?
+            $filter('filter')(data, params.filter()) :
+            data;
         var orderedData = params.sorting() ?
-            $filter('orderBy')(data, params.orderBy()) :
+            $filter('orderBy')(filteredData, params.orderBy()) :
             data;
 
+        params.total(orderedData.length); // set total for recalc pagination
         $defer.resolve(orderedData.slice((params.page() - 1) * params.count(), params.page() * params.count()));
       }
-    });
+      });
   };
 });
 
